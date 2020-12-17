d3d8fd3eb510d54f4795aa3545a1509917874d2a
anlycchen <kimythanly@gmail.com>
2020-12-17 21:24:26 +0800

    add requirements in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tqdmX", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="KimythAnly",
     author_email="kimythanly@gmail.com",
     description="A tqdm wrapper for multi-line logging.",
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kimythanly/tqdmX",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'tqdm',
+        'addict'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
